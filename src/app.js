e2483c7a4956ebdae5f6c2b09a06d0bd1f67858f
Fredrik <fredrik.christenson@gmail.com>
2017-10-04 20:07:25 +0200

    added static cache

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public"), { maxAge: "1y" }));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
