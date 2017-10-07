2486244c4906f97550935c1e1992c1e4d2632477
Fredrik <fredrik.christenson@gmail.com>
2017-10-07 16:53:29 +0200

    fix

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,13 +1,15 @@
 (() => {
   const jsSelector = str => `[data-js="${str}"]`;
   const menuButton = document.querySelector(jsSelector("menu-toggle"));
-  const menuClickCatcher = document.querySelector(
+  const menuClickCatchers = document.querySelectorAll(
     jsSelector("menu-click-catcher")
   );
 
-  if (!menubar || !menuClickCatcher) return;
+  if (!menubar || !menuClickCatchers) return;
 
-  menuClickCatcher.addEventListener("click", event => {
-    menuButton.checked = false;
+  menuClickCatchers.forEach(e => {
+    e.addEventListener("click", event => {
+      menuButton.checked = false;
+    });
   });
 })();
