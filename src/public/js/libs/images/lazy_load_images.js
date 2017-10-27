a112984636034891546055ad2373521d37a8a365
Fredrik <fredrik.christenson@gmail.com>
2017-10-27 23:51:10 +0200

    fix

--- src/public/js/libs/images/lazy_load_images.js
+++ src/public/js/libs/images/lazy_load_images.js
@@ -2,14 +2,14 @@ const jsSelector = require("../selectors").jsSelector;
 
 var hasScrolled = false;
 
-window.addEventListener("scroll", function loadImages() {
+function loadImages() {
   if (hasScrolled) {
     document.removeEventListener("scroll", loadImages);
     return;
   }
 
   hasScrolled = true;
-  const images = document.querySelectorAll("img[data-src]");
+  const images = document.querySelectorAll("img[data-js=load-on-scroll]");
 
   const button = document.getElementById("menu-button");
   button.classList.add("menu-button-background");
@@ -21,4 +21,19 @@ window.addEventListener("scroll", function loadImages() {
       img.classList.remove("header-image");
     };
   });
+}
+
+window.addEventListener("scroll", loadImages);
+
+const images = document.querySelectorAll("img[data-js=lazy-load]");
+
+const button = document.getElementById("menu-button");
+button.classList.add("menu-button-background");
+
+Array.from(images).forEach(function(img) {
+  img.setAttribute("src", img.getAttribute("data-src"));
+  img.onload = function() {
+    img.classList.add("is-visible");
+    img.classList.remove("header-image");
+  };
 });
